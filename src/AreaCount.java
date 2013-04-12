51d06868f6d52a16ff43651e20aa7305783624da
yume190 <yume190@gmail.com>
2013-04-12 23:00:10 +0800

    about 90% complete

--- src/AreaCount.java
+++ src/AreaCount.java
@@ -8,6 +8,8 @@ public class AreaCount {
 	private int wid;
 	//格子長和寬的單位
 	private double scale;
+	//
+	private int optimizationFlag;
 	
 	//the bound of the circle (up down left right)
 	//圓的邊界 (上下左右)
@@ -15,6 +17,8 @@ public class AreaCount {
 	private int downBound;
 	private int leftBound;
 	private int rightBound;
+	
+	private Point centerOfNewCircle;
 	
 	AreaCount(int length,int width,double scale){
 		this.scale = scale;
@@ -29,17 +33,31 @@ public class AreaCount {
 		this.scope = new int[len][wid];
 	}
 	
+	////////////////////////////////////////////////////////////////
+	public void show(){
+		this.scope[0][9]=1;
+		System.out.println(this.upBound);
+		System.out.println(this.downBound);
+		System.out.println(this.leftBound);
+		System.out.println(this.rightBound);
+		for(int a = this.scope.length-1; a >= 0 ;a--){
+			for(int b = 0;b < this.scope[a].length;b++){
+				System.out.print(scope[b][a]);
+			}
+			System.out.println();
+		}
+	}
+	
 	public void addCircle(double xx,double yy,double rad){
 		double centerX = xx / scale;
 		double centerY = yy / scale;
 		double radius = rad / scale;
 		
+		this.centerOfNewCircle = new Point(xx,yy);
+		
 		setBound(centerX,centerY,radius);
 		
-		
-		
-		
-		
+		paintCircle(centerX,centerY,radius);
 	}
 	
 	private void setBound(double centerX,double centerY,double radius){
@@ -49,53 +67,91 @@ public class AreaCount {
 		setRightBound(centerX,centerY,radius);
 	}
 	
+	//at 10 * 10 area
+	//put a circle (5,5) and radius is 5
+	//then y + r > wid
+	//5 + 5 > 10
+	//bound -> 9
 	private void setUpBound(double centerX,double centerY,double radius){
-		if ((int)(Math.floor(centerY + radius)) > wid)
+		if ((int)(Math.floor(centerY + radius)) > wid - 1)
 			upBound = wid - 1;
 		else
-			upBound = (int)(Math.floor(centerY));
+			upBound = (int)(Math.floor(centerY + radius));
 	}
 
 	private void setDownBound(double centerX,double centerY,double radius){
 		if ((int)(Math.floor(centerY - radius)) < 0)
 			downBound = 0;
 		else
-			downBound= (int)(Math.floor(centerY));
+			downBound= (int)(Math.floor(centerY -  radius));
 	}
 	
 	private void setLeftBound(double centerX,double centerY,double radius){
 		if ((int)(Math.floor(centerX - radius)) < 0)
 			leftBound = 0;
 		else
-			leftBound= (int)(Math.floor(centerX));
+			leftBound= (int)(Math.floor(centerX - radius));
 	}
 	
 	private void setRightBound(double centerX,double centerY,double radius){
-		if ((int)(Math.floor(centerX + radius)) > len)
+		if ((int)(Math.floor(centerX + radius)) > len - 1)
 			rightBound = len - 1;
 		else
-			rightBound = (int)(Math.floor(centerX));
+			rightBound = (int)(Math.floor(centerX + radius));
 	}
 	
-	public Point getLU(double xx, double yy){
-		return new Point(xx*scale,yy*(scale+1));
+	private void paintCircle(double centerX,double centerY,double radius){
+		paintCircleLU(centerX,centerY,radius);
+		paintCircleRU(centerX,centerY,radius);
+		paintCircleLD(centerX,centerY,radius);
+		paintCircleRD(centerX,centerY,radius);
 	}
 	
-	public Point getLD(double xx, double yy){
+	private void paintCircleLU(double centerX,double centerY,double radius){
+		for(int yStart = upBound;yStart >= (int)centerY;yStart--){
+			//////////////////////////////////-1
+			for(int xStart = (int)centerX - 1;xStart >= leftBound;xStart--){
+				if(inCircle(this.getLeftUpPoint(xStart, yStart),radius))
+					this.scope[xStart][yStart] += 1;
+				//else
+					//break;
+			}
+		}
+	}
+	private void paintCircleRU(double centerX,double centerY,double radius){}
+	private void paintCircleLD(double centerX,double centerY,double radius){}
+	private void paintCircleRD(double centerX,double centerY,double radius){}
+	
+	
+	private Point getLeftUpPoint(double xx, double yy){
+		return new Point(xx*scale,yy*scale+scale);
+	}
+	
+	private Point getLeftDownPoint(double xx, double yy){
 		return new Point(xx*scale,yy*scale);
 	}
 	
-	public Point getRU(double xx, double yy){
-		return new Point(xx*(scale+1),yy*(scale+1));
+	private Point getRightUpPoint(double xx, double yy){
+		return new Point(xx*scale+scale,yy*scale+scale);
 	}
 	
-	public Point getRD(double xx, double yy){
-		return new Point(xx*(scale+1),yy*scale);
+	private Point getRightDownPoint(double xx, double yy){
+		return new Point(xx*scale+scale,yy*scale);
+	}
+	
+	private boolean inCircle(Point zone,double c){
+		double a = Math.pow(this.centerOfNewCircle.getX() - zone.getX() ,2);
+		double b = Math.pow(this.centerOfNewCircle.getY() - zone.getY() ,2);
+		c = c * c;
+		if(a + b <= c)
+			return true;
+		else
+			return false;
 	}
 	
 	
 	
-	private class Point{
+	public class Point{
 		
 		private double x;
 		private double y;
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+//import java.util.ArrayList;
+
+public class Test {
+	public static void main (String[] args)  {
+		AreaCount AC = new AreaCount(10,10,1);
+		AC.addCircle(4, 4, 5);
+		AC.show();
+	}  
+}
